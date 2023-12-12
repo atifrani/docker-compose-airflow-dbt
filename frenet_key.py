6b38a12eb8629d32f9f8eb0af41478e11a542ae5
Axel Tifrani <axel@logbrain.fr>
2023-12-12 14:46:43 +0100

    add env airflow variables

--- frenet_key.py
+++ frenet_key.py
@@ -3,10 +3,12 @@ import configparser
 
 # Generate a random Fernet key
 fernet_key = Fernet.generate_key().decode()
-
+print (fernet_key)
+'''
 # Store the key
 config = configparser.ConfigParser()
 config.read(‘/root/airflow/airflow.cfg’)
 config[‘core’][‘fernet_key’] = fernet_key
 with open(‘/root/airflow/airflow.cfg’, ‘w’) as f:
    config.write(f)
+'''
