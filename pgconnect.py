a669f4b8b1499fa8116fd7521da6e5fb3e8c617f
Axel Tifrani <axel@logbrain.fr>
2023-12-12 11:54:09 +0100

    encrypt sensible data with frenet key

--- pgconnect.py
+++ pgconnect.py
@@ -5,8 +5,9 @@ import configparser
 config = configparser.ConfigParser()
 config.read('/root/airflow/airflow.cfg')
 
-# Store the URL of the MySQL database
-config['core']['sql_alchemy_conn'] = 'postgresql+psycopg2://{user}:{password}@postgres/{db}'.format(user=env.get('POSTGRES_USER'), password=env.get('POSTGRES_PASSWORD'), db=env.get('POSTGRES_DATABASE'))
-config['core']['executor'] = 'LocalExecutor'
+# Store the URL of the Postgresql database
+config['core']['sql_alchemy_conn'] = 'postgresql+psycopg2://{user}:{password}@postgres/{db}'.format(user=env.get('POSTGRES_USER'), password=env.get('POSTGRES_PASSWORD'), db=env.get('POSTGRES_DB'))
+#config['core']['executor'] = 'LocalExecutor'
+config['core']['executor'] = 'CeleryExecutor'
 with open('/root/airflow/airflow.cfg', 'w') as f:
      config.write(f)  
