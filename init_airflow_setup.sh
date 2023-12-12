8725607f8e5fc22b067ba473b61c1e7846c48437
Axel Tifrani <axel@logbrain.fr>
2023-12-12 11:39:49 +0100

    add airflow admin credentials

--- init_airflow_setup.sh
+++ init_airflow_setup.sh
@@ -12,7 +12,7 @@ if [ $IS_INITDB ]; then
   echo "Initializing Airflow DB setup and Admin user setup because value of IS_INITDB is $IS_INITDB"
   echo " Airflow admin username will be $AIRFLOW_USER"
 
-  docker exec -ti airflow_webserver_cont airflow db migrate && echo "Initialized airflow DB"
+  docker exec -ti airflow_webserver_cont airflow db init && echo "Initialized airflow DB"
   docker exec -ti airflow_webserver_cont airflow users create --role Admin --username $AIRFLOW_USER --password $AIRFLOW_PASSWORD -e $AIRFLOW_USER_EMAIL -f airflow -l airflow && echo "Created airflow Initial admin user with username $AIRFLOW_USER"
 
 else
