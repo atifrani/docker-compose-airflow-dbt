eafd2edf138694ba94b6af798a0a258e8e794623
Axel Tifrani <axel@logbrain.fr>
2023-12-13 08:48:54 +0100

    update volumes

--- frenet_key.py
+++ frenet_key.py
@@ -4,11 +4,10 @@ import configparser
 # Generate a random Fernet key
 fernet_key = Fernet.generate_key().decode()
 print (fernet_key)
-'''
+
 # Store the key
 config = configparser.ConfigParser()
 config.read(‘/root/airflow/airflow.cfg’)
 config[‘core’][‘fernet_key’] = fernet_key
 with open(‘/root/airflow/airflow.cfg’, ‘w’) as f:
    config.write(f)
-'''
